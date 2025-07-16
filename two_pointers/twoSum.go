a0dcb07562c103a9df0513749482dd2dedfbb96d
Hasib <mdhsbaskari@gmail.com>
2025-07-16 22:31:55 +0600

    Refactor TwoSum function to improve variable naming and return value for no solution

--- two_pointers/twoSum.go
+++ two_pointers/twoSum.go
@@ -1,20 +1,5 @@
 package twopointers
 
-<<<<<<< HEAD
-func TwoSum(numbers []int, target int) []int {
-	left, right := 0, len(numbers)-1
-	for left < right {
-		sum := numbers[left] + numbers[right]
-		if sum == target {
-			return []int{left + 1, right + 1}
-		} else if sum < target {
-			left++
-		} else {
-			right--
-		}
-	}
-	return []int{-1, -1} // Return -1 if no solution is found
-=======
 func TwoSum(nums []int, target int) []int {
 	i := 0
 	j := len(nums) - 1
@@ -28,5 +13,4 @@ func TwoSum(nums []int, target int) []int {
 		}
 	}
 	return []int{}
->>>>>>> c38258f (Add threeSum and twoSum implementations with input handling and launch configuration)
 }
