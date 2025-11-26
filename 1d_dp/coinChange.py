5354bae6bb04e5b12be4fd5ce58a00c45ec4412e
Hasib <mdhsbaskari@gmail.com>
2025-11-26 15:38:31 +0600

    Implement solution for the "Coin Change" problem

---
+++ 1d_dp/coinChange.py
@@ -0,0 +1,37 @@
+def solve(coins: list[int], amount: int) -> int:
+    dp = {}
+
+    count = 0
+    def dfs(amnt) -> int:
+        nonlocal count
+        print(count)
+        count += 1
+        # if count > 6500: return 0
+        # else: count += 1
+        if amnt == 0:
+            return 0
+        if amnt in dp:
+            return dp[amnt]
+        
+        res = float('inf')
+        for coin in coins:
+            if amnt - coin >= 0:
+                res = min(res, 1 + dfs(amnt-coin))
+                print(f"amnt: {amnt}, coin: {coin}, dp: {dp}")
+        
+        dp[amnt] = res
+        return dp[amnt]
+    minCoins = dfs(amount)
+    return -1 if minCoins >= float('inf') else minCoins
+
+if __name__ == "__main__":
+    tests = [
+        ([1,2,5], 11),
+        ([2], 3),
+        ([1], 0),
+        ([186,419,83,408], 6249),
+    ]
+    for test in tests:
+        print(f"Input: {test}")
+        print(f"Output: {solve(*test)}")
+        print("-" * 20)
